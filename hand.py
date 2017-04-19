d5a26dc0c4f078741b03a84883be1af05535a934
omidahomi <oiqbal@sjnma.org>
2017-04-19 11:00:37 -0500

    [almost done with hand.py and started main.py]

---
+++ hand.py
@@ -0,0 +1,18 @@
+import random 
+from card import Card
+class Hand:
+	def __init__(self):
+		self.value = []
+		self.validSuits=["Clubs","Spades","Hearts","Diamonds"]
+		self.validRanks=["2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"]
+		for suit in self.validSuits:
+			for rank in self.validRanks:
+			    self.value.append(Card(rank,suit))
+    def Cut(self) :
+        first = Hand()
+        second = Hand()
+        halfway = len(self.value)/2
+        first.value = self.value[halfway:]
+        second.value=self.value[:halfway]
+        self.value = []
+        return first,second
